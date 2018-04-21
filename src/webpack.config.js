96670dacf6854ec31f389a78ed32ced42de19131
Mathieu DECROOCQ <mathieu.decroocq@econocom.com>
2018-04-21 11:03:02 +0200

    add sass extraction

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,37 +1,41 @@
 const webpack = require("webpack");
 const path = require("path");
+const extractTextWebpackPlugin = require("extract-text-webpack-plugin");
+
 
 let config = {
-    entry: "./app/index.js",
-    output: {
-      path: path.resolve(__dirname, "./wwwroot"),
-      filename: "bundle.js"
-    },
-    module: {
-      rules: [
-
+  entry: "./app/index.js",
+  output: {
+    path: path.resolve(__dirname, "./wwwroot"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [{
         //babel plugin
-        {
-          test: /\.js$/,
-          exclude: /(node_modules)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['env'],
-              babelrc: false
-            }
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+            babelrc: false
           }
-        },
-
-        //sass plugin
-        {
-          test: /\.scss$/,
-          loader: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader']
         }
-      ]
-    }
-  }
-  
-  module.exports = config;
+      },
+      {
+        //sass plugin
+        test: /\.scss$/,
+        use: extractTextWebpackPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }]
+  },
+  plugins: [
+    new extractTextWebpackPlugin("styles.css")
+  ]
+}
+
+module.exports = config;
 
   //https://www.alsacreations.com/tuto/lire/1754-debuter-avec-webpack.html
